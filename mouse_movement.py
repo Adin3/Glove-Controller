a3386ec4340ffef6c34bccca699d95e3199cc74f
Adrian <neacsu_adrian@yahoo.com>
2025-05-24 20:02:23 +0300

    Mouse Movement
    added Mouse movement

---
+++ mouse_movement.py
@@ -0,0 +1,14 @@
+import pyautogui
+
+class MouseMovement:
+    def __init__(self):
+        self.resolution = pyautogui.size()
+        self.x, self.y = pyautogui.position()
+        self.is_close_app = False
+        pyautogui.moveTo(self.resolution[0] / 2, self.resolution[1] / 2)
+    
+    def change_min_time(self, time):
+        pyautogui.MINIMUM_DURATION = time
+
+    def move(self, posX, posY, time = 0):
+        pyautogui.moveRel(posX, posY, time)
