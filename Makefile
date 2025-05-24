d5228b5e3df89748e212b052e4286c8965dc3a2d
Adrian <neacsu_adrian@yahoo.com>
2025-05-24 15:18:01 +0300

    UART HC-05
    Communication with HC-05 through UART

--- Makefile
+++ Makefile
@@ -3,9 +3,10 @@ F_CPU = 16000000UL
 CC = avr-gcc
 OBJCOPY = avr-objcopy
 CFLAGS = -Wall -Os -mmcu=$(MCU) -DF_CPU=$(F_CPU)
+LDFLAGS = -Wl,-u,vfprintf -lprintf_flt -lm
 
 main.hex: main.c
-	$(CC) $(CFLAGS) -o main.elf main.c
+	$(CC) $(CFLAGS) $(LDFLAGS) -o main.elf main.c
 	$(OBJCOPY) -O ihex -R .eeprom main.elf main.hex
 
 flash: main.hex
--- main.c
+++ main.c
@@ -1,11 +1,55 @@
+#define F_CPU 16000000UL
 #include <avr/io.h>
 #include <util/delay.h>
+#include <string.h>
+
+#define BUFFER_SIZE 64
+
+void uart_init(void) {
+    uint16_t ubrr = 103;
+    UBRR0H = (ubrr >> 8);
+    UBRR0L = ubrr;
+    UCSR0B = (1 << RXEN0) | (1 << TXEN0);
+    UCSR0C = (1 << UCSZ01) | (1 << UCSZ00);
+}
+
+void uart_send_char(char c) {
+    while (!(UCSR0A & (1 << UDRE0)));
+    UDR0 = c;
+}
+
+void uart_send_string(const char* str) {
+    while (*str) {
+        uart_send_char(*str++);
+    }
+}
+
+char uart_receive_char(void) {
+    while (!(UCSR0A & (1 << RXC0)));
+    return UDR0;
+}
+
+void uart_receive_line(char* buffer, uint8_t maxlen) {
+    uint8_t i = 0;
+    char c;
+    while (i < maxlen - 1) {
+        c = uart_receive_char();
+        if (c == '\r') continue;
+        if (c == '\n') break;
+        buffer[i++] = c;
+    }
+    buffer[i] = '\0';
+}
 
 int main(void) {
-    DDRB |= (1 << PB5); // Set digital pin 13 as output
+    char buffer[BUFFER_SIZE];
+    uart_init();
+    _delay_ms(1000);
 
     while (1) {
-        PORTB ^= (1 << PB5); // Toggle pin 13
-        _delay_ms(1500);
+        uart_receive_line(buffer, BUFFER_SIZE);
+        uart_send_string("");
+        uart_send_string(buffer);
+        uart_send_string("\r\n");
     }
 }
